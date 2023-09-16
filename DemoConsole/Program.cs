cbd20e4ee739ec4ed39163f395c5bba42bdf25d4
Daan Acohen <daan.acohen@connectingapps.net>
2023-09-16 15:56:58 +0200

    Added the project with solution

---
+++ DemoConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DemoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simulating a financial operation where we're adding a small amount multiple times
+            const int iterations = 10000;
+            const decimal decimalValue = 0.01M; // M suffix denotes a decimal
+            const double doubleValue = 0.01;
+
+            decimal totalDecimal = 0M;
+            double totalDouble = 0.0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                totalDecimal += decimalValue;
+                totalDouble += doubleValue;
+            }
+
+            Console.WriteLine($"Using decimal after {iterations} iterations: {totalDecimal}"); // Expected: 100
+            Console.WriteLine($"Using double after {iterations} iterations: {totalDouble}"); // Might not be exactly 100 due to precision errors
+
+            if ((double)totalDecimal != totalDouble)
+            {
+                Console.WriteLine("The values are not the same!");
+            }
+            else
+            {
+                Console.WriteLine("The values are the same.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
